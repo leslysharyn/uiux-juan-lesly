a1cf1ccfc073cd9cf67dade7b22e199b1dc28762
PinedaJuan98 <juan.pineda@uniandes.edu.co>
2023-03-12 20:56:14 -0500

    login web created

--- MaquetacionWeb/mediaAlarmWeb/src/app/app-routing.module.ts
+++ MaquetacionWeb/mediaAlarmWeb/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyAlarmsComponent } from './my-alarms/my-alarms.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'my-alarms', component: MyAlarmsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- MaquetacionWeb/mediaAlarmWeb/src/app/app.component.ts
+++ MaquetacionWeb/mediaAlarmWeb/src/app/app.component.ts
@@ -4,7 +4,7 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit{
   title = 'mediaAlarmWeb';
