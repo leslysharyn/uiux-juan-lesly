08c9829e71ab7fbf033f19559e803ccb5c222573
PinedaJuan98 <juan.pineda@uniandes.edu.co>
2023-03-13 16:44:25 -0500

    Creación pantallas web + menu navegación completado

--- MaquetacionWeb/mediaAlarmWeb/src/app/app-routing.module.ts
+++ MaquetacionWeb/mediaAlarmWeb/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyAlarmsComponent } from './my-alarms/my-alarms.component';
 import { LoginComponent } from './login/login.component';
+import { MultimediaComponent } from './multimedia/multimedia.component';
+import { AjustesComponent } from './ajustes/ajustes.component';
+import { SyncComponent } from './sync/sync.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'my-alarms', component: MyAlarmsComponent }
+  { path: 'my-alarms', component: MyAlarmsComponent },
+  { path: 'multimedia', component: MultimediaComponent },
+  { path: 'settings', component: AjustesComponent },
+  { path: 'sync', component: SyncComponent }
 ];
 
 @NgModule({
--- MaquetacionWeb/mediaAlarmWeb/src/app/app.module.ts
+++ MaquetacionWeb/mediaAlarmWeb/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyAlarmsComponent } from './my-alarms/my-alarms.component';
 import { LoginComponent } from './login/login.component';
+import { MultimediaComponent } from './multimedia/multimedia.component';
+import { AjustesComponent } from './ajustes/ajustes.component';
+import { SyncComponent } from './sync/sync.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyAlarmsComponent,
-    LoginComponent
+    LoginComponent,
+    MultimediaComponent,
+    AjustesComponent,
+    SyncComponent
   ],
   imports: [
     BrowserModule,
