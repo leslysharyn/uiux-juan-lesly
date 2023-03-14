51b76c76ac07cdfacedae3de9681c6c03bf9403a
leslysharyn <leslysharyncj@gmail.com>
2023-03-13 23:05:55 -0500

    Crear Alarma parte 2

--- MaquetacionMobile/app/src/main/java/com/example/mediaalarm/CreateEditAlarma.kt
+++ MaquetacionMobile/app/src/main/java/com/example/mediaalarm/CreateEditAlarma.kt
@@ -2,10 +2,17 @@ package com.example.mediaalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mediaalarm.databinding.ActivityCreateEditAlarmaBinding
 
 class CreateEditAlarma : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateEditAlarmaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_edit_alarma)
+        binding = ActivityCreateEditAlarmaBinding.inflate(layoutInflater)
+        val view = binding.root.also {
+            setContentView(it)
+        }
     }
+
 }
